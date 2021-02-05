9e2f2b920ddeeadb4a10e1576c7fbb860aa4c292
legarrod <samluis292@gmail.com>
2021-02-05 11:22:18 -0500

    primer commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <p className="text-5xl">Casa</p>
+    </div>
+  )
+}
